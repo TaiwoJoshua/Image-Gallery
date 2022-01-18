57f0856245f1d9c14ab9fc1fa3947b20713685d9
teejayo7 <joshuataiwo07@gmail.com>
2022-01-18 10:33:11 +0100

    I removed editorial, following and categories from my sidenav

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,5 +1,11 @@
 const displayBlock = document.getElementById('blockdisplay');
 const logouts = document.getElementById('logouts');
+const invisible2 = document.getElementsByClassName('invisible2');
+const invisible = document.getElementsByClassName('invisible');
+const visible = document.getElementsByClassName('visible');
+const puser = document.getElementsByClassName('puser');
+const usemail = document.getElementsByClassName('usemail');
+const pnumber = document.getElementsByClassName('pnumber');
 
 
 function myFunction(){
@@ -26,5 +32,43 @@ function logout(){
     logouts.style.zIndex = "4";
 }
 
+function searchFunction(){
+    let input = document.getElementById('search').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('inputs');
+    
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+            document.getElementById('notFound').style.display = "block";
+        }
+        else {
+            x[i].style.display="block";     
+            document.getElementById('notFound').style.visibility = "hidden";            
+        }
+    }
+  }
+  
+function phoneFunction(){
+    invisible2.className = "visible";
+    visible.className = "invisible2";
+    puser.className = "visible";
+    pnumber.style.backgroundColor = "#22046b";
+}
+
+function emailFunction(){
+    invisible.className = "visible";
+    visible.className = "invisible";
+    puser.className = "visible";
+    usemail.style.backgroundColor = "#22046b";
+}
+
+function userFunction(){
+    usemail.className = "invisible";
+    invisible.className = "invisible";
+    invisible2.className = "invisible2";
+    visible.className = "visible";
+    puser.style.backgroundColor = "#22046b";
+}
 
 
